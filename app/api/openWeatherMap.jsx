f52295e9f975dc27b4e18a1e7f51dcf2badef485
Sushanth <sushanth94@gmail.com>
2017-03-21 01:34:33 +0530

    additional changes to index

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -6,17 +6,15 @@ module.exports = {
   getTemp : function(location){
     var encodedLocation = encodeURIComponent(location);
     var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
-
     return axios.get(requestUrl).then(function(response){
-      debugger;
       if(response.data.cod && response.data.message){
         throw new Error(response.data.message);
       }
       else{
         return response.data.main.temp;
       }
-    },function(error){
-      throw new Error(error.data.message);
+    },function(invalid){
+      throw new Error(invalid.response.data.message);
     });
   }
 };
