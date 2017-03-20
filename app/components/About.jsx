1842dbdc313f0ad5cc8e03828d5c2c2827266e73
Sushanth <sushanth94@gmail.com>
2017-03-21 00:43:06 +0530

    styles added to about and examples page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,10 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-    <h3>About</h3>
-    <p>Welcome to About page!</p>
+    <h1 className="text-center">About</h1>
+      <p>Gather weather data for a given location from <a href="http://openweathermap.org">OpenWeatherMap</a>.</p>
+
+      <p>If a misspelled location is entered, an attempt is made to interpret the desired location.</p>
     </div>
   );
 };
